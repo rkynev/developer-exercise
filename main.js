1e27a971bfc58c8c8be63d0d19a0b19b7828667a
Rumen Kynev <rkynev@gmail.com>
2020-02-10 22:08:26 +0000

    make it better

--- main.js
+++ main.js
@@ -1,46 +1,98 @@
+//Bag to be made mobile friendly
+//To add display logic for bag
 
-const ul = document.getElementById('dresses');
-fetch('https://api.myjson.com/bins/evnom')
-    .then((resp) => resp.json())
-    .then(data => {
+let loading;
+function spinner() {
+  loading = setTimeout(showPage, 1500);
+}
 
-        let finalData = data.hits.filter(img => img.image)
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("show").style.display = "block";
+}
 
-         finalData.map(dress => {
+const ul = document.getElementById("dresses");
+const bag = document.getElementById("bag");
 
-                    function createElements(element) {
-                        return document.createElement(element);
-                    }
-                            let li = createElements('li'),
-                                img = createElements('img'),
-                                span = createElements('span');
-                                newSpan = createElements('p');
-                                button = createElements('button')
-                            img.src = dress.image.link;
-                            span.innerHTML = `${dress.product_name}`;
-                            newSpan.innerHTML = `£${dress.price}.00`;
-                            button.innerHTML = 'add to bag';
-            
-            function appendEl(parent, element) {
-                return parent.appendChild(element);
-            }
-            appendEl(li, img);
-            appendEl(li, span);
-            appendEl(li, newSpan);
-            appendEl(li, button);
-            appendEl( ul, li);
-            
+function createElements(element) {
+  return document.createElement(element);
+}
 
-        })
-    })
-    .catch(error => 
-        console.log(JSON.stringify(error))
-    );
+function appendEl(parent, element) {
+  return parent.appendChild(element);
+}
 
+fetch("https://api.myjson.com/bins/evnom")
+  .then(resp => resp.json())
+  .then(data => {
+    let finalData = data.hits.filter(img => img.image);
 
+    finalData.map(dress => {
+      let li = createElements("li"),
+        img = createElements("img"),
+        span = createElements("span");
+      newSpan = createElements("p");
+      button = createElements("button");
+      img.src = dress.image.link;
+      span.innerHTML = `${dress.product_name}`;
+      newSpan.innerHTML = `£${dress.price}.00`;
+      button.innerHTML = "add to bag";
+      productId = dress.product_id;
+      button.setAttribute("class", "product-button");
+      button.setAttribute("data-id", productId);
 
+      appendEl(li, img);
+      appendEl(li, span);
+      appendEl(li, newSpan);
+      appendEl(li, button);
+      appendEl(ul, li);
+    });
 
+    let buttons = document.getElementsByClassName("product-button");
 
+    Array.from(buttons).forEach(element => {
+      element.addEventListener("click", addToBag);
+    });
+    console.log(buttons);
+    function addToBag(event) {
+      let productId = event.target.getAttribute("data-id");
+      for (let i = 0; i < finalData.length; i++) {
+        const dress = finalData[i];
 
+        if (productId == dress.product_id) {
+          let div = createElements("div"),
+            img = createElements("img"),
+            span = createElements("span");
+          price = createElements("p");
+          button = createElements("button");
+          input = createElements("input");
+          img.src = dress.image.link;
+          span.innerHTML = `${dress.product_name}`;
+          price.innerHTML = `£${dress.price}.00`;
+          button.innerHTML = "Remove";
+          productId = dress.product_id;
+          input.setAttribute("value", 1);
+          input.setAttribute("type", "number");
+          div.setAttribute("class", "bag-product");
+          span.setAttribute("class", "bag-product-name");
+          img.setAttribute("class", "bag-img");
+          button.setAttribute("class", "danger-button");
+          button.setAttribute("data-id", productId);
+          price.setAttribute("class", "price");
 
+          appendEl(div, img);
+          appendEl(div, span);
+          appendEl(div, price);
+          appendEl(div, input);
+          appendEl(div, button);
+          appendEl(bag, div);
 
+          console.log(dress.image.link);
+        }
+      }
+
+      let list = document.getElementsByClassName("bag-product");
+      document.getElementById("total-count").innerText = list.length;
+    }
+  })
+  .catch(error => console.log(JSON.stringify(error)));
