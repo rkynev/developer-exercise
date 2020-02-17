d6e6cc86d6f33a36830b0d43e4f63db3f4762bbd
Rumen Kynev <rkynev@gmail.com>
2020-02-17 20:01:54 +0000

    bag logic done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-//TO ADD DISPLAY LOGIC TO BAG - work in progress
+//TO ADD DISPLAY LOGIC TO BAG - done
 //BAG AND HDR TO BE MADE MOBILE FRIENDLY - work in progress
 //TO ADD DISPLAY NONE WHEN BAG IS EMPTY - work in progress
-//TO ADD TOTAL TO BAG - work in progress
+//TO ADD TOTAL TO BAG - done
 //Bag Counter - done
 //Remove Button - done
 
@@ -16,7 +16,7 @@ function showPage() {
 }
 
 const ul = document.getElementById("dresses");
-const bag = document.getElementById("bag");
+const bag = document.querySelector(".bag-dresses");
 
 function createElements(element) {
   return document.createElement(element);
@@ -53,94 +53,99 @@ fetch("https://api.myjson.com/bins/evnom")
     });
 
     let buttons = document.getElementsByClassName("product-button");
-
     Array.from(buttons).forEach(element => {
       element.addEventListener("click", addToBag);
     });
 
     function addToBag(event) {
       let productId = event.target.getAttribute("data-id");
-      
-            for (let i = 0; i < finalData.length; i++) {
-              const dress = finalData[i];
-              if (productId == dress.product_id) {
-                let div = createElements("div"),
-                  img = createElements("img"),
-                  span = createElements("span");
-                price = createElements("p");
-                button = createElements("button");
-                input = createElements("input");
-                img.src = dress.image.link;
-                span.innerHTML = `${dress.product_name}`;
-                price.innerHTML = `£${dress.price}.00`;
-                button.innerHTML = "Remove";
-                productId = dress.product_id;
-                input.setAttribute("value", 1);
-                input.setAttribute("type", "number");
-                input.setAttribute("class", "bag-quantity-input");
-                div.setAttribute("class", "bag-product");
-                span.setAttribute("class", "bag-product-name");
-                img.setAttribute("class", "bag-img");
-                button.setAttribute("class", "danger-button");
-                button.setAttribute("data-id", productId);
-                price.setAttribute("class", "price");
+      let itemInBag = document.getElementsByClassName("bag-product");
+      elements = Array.from(itemInBag);
+      bagData = elements
+        .map(element => element.children[4].attributes[1].value)
+        .filter(el => el == productId);
 
-                appendEl(div, img);
-                appendEl(div, span);
-                appendEl(div, price);
-                appendEl(div, input);
-                appendEl(div, button);
-                appendEl(bag, div);
-                uptdateBagIconTotal();
-                totalItems();
-              }
+      for (let i = 0; i < finalData.length; i++) {
+        const dress = finalData[i];
 
-            } 
+        if (productId == dress.product_id && productId == bagData) {
+          alert("Item already in Bag");
+        } else if (productId == dress.product_id) {
+          let div = createElements("div"),
+            img = createElements("img"),
+            span = createElements("span");
+          price = createElements("p");
+          button = createElements("button");
+          input = createElements("input");
+          img.src = dress.image.link;
+          span.innerHTML = `${dress.product_name}`;
+          price.innerHTML = `£${dress.price}.00`;
+          button.innerHTML = "Remove";
+          productId = dress.product_id;
+          input.setAttribute("value", 1);
+          input.setAttribute("type", "number");
+          input.setAttribute("class", "bag-quantity-input");
+          div.setAttribute("class", "bag-product");
+          span.setAttribute("class", "bag-product-name");
+          img.setAttribute("class", "bag-img");
+          button.setAttribute("class", "danger-button");
+          button.setAttribute("data-id", productId);
+          price.setAttribute("class", "price");
 
-            let quantityInputs = document.getElementsByClassName('bag-quantity-input');
-                Array.from(quantityInputs).forEach(element => {
-                  element.addEventListener("change", increaseTotal);
-                });
+          appendEl(div, img);
+          appendEl(div, span);
+          appendEl(div, price);
+          appendEl(div, input);
+          appendEl(div, button);
+          appendEl(bag, div);
+          uptdateBagIconTotal();
+          totalItems();
+        }
+      }
 
-            function increaseTotal(){
-              //to add some logic
-            }
+      let quantityInputs = document.getElementsByClassName(
+        "bag-quantity-input"
+      );
+      Array.from(quantityInputs).forEach(element => {
+        element.addEventListener("change", totalItems);
+      });
 
-            function totalItems() {
-            
-              let bagItem = document.getElementsByClassName("bag-product");
-                elements = Array.from(bagItem);
-                dataBag = elements.map(element => 
-                  parseFloat( element.children[2].innerText.replace("£", ""))
-                ).reduce((prev, next) => prev + next)
-                document.querySelector(".inner-total").innerText = `£${dataBag}.00`;
-                console.log(dataBag)          
-            }
-            
+      function totalItems() {
+        let bagItem = document.getElementsByClassName("bag-product");
+        elements = Array.from(bagItem);
+        dataBag = elements.map(element => ({
+          price: parseFloat(element.children[2].innerText.replace("£", "")),
+          qty: parseFloat(element.children[3].value)
+        }));
+        let total = dataBag.map(img => img.price * img.qty);
+        let grandTotal = total.reduce((a, b) => a + b);
+        document.querySelector(".inner-total").innerText = `£${grandTotal}.00`;
+      }
 
-            let removeBagItemButton = document.getElementsByClassName("danger-button");
-            for (let i = 0; i < removeBagItemButton.length; i++) {
-              let button = removeBagItemButton[i];
-              button.addEventListener("click", removeBagItem);
-            }
-            function removeBagItem(event) {
-              let bagItem = document.getElementsByClassName("bag-product");
-              let buttonClicked = event.target;
-              buttonClicked.parentElement.remove();
-              uptdateBagIconTotal();
-              if(bagItem.length == 0){
-                document.querySelector(".inner-total").innerText = `£0.00`;
-              }else{
-                totalItems();
-              }
-                
-            }
-            function uptdateBagIconTotal() {
-              let list = document.getElementsByClassName("bag-product");
-              document.getElementById("total-count").innerText = list.length;
-            }
-       //edn of addToBag     
+      let removeBagItemButton = document.getElementsByClassName(
+        "danger-button"
+      );
+      for (let i = 0; i < removeBagItemButton.length; i++) {
+        let button = removeBagItemButton[i];
+        button.addEventListener("click", removeBagItem);
+      }
+
+      function removeBagItem(event) {
+        let bagItem = document.getElementsByClassName("bag-product");
+        let buttonClicked = event.target;
+        buttonClicked.parentElement.remove();
+        uptdateBagIconTotal();
+        if (bagItem.length == 0) {
+          document.querySelector(".inner-total").innerText = `£0.00`;
+        } else {
+          totalItems();
+        }
+      }
+
+      function uptdateBagIconTotal() {
+        let list = document.getElementsByClassName("bag-product");
+        document.getElementById("total-count").innerText = list.length;
+      }
     }
-
   })
   .catch(error => console.log(JSON.stringify(error)));
